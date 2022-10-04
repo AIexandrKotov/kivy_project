9d71b377c622abcbba23008abea47a9cab0c3582
Kotov <44296606+AIexandrKotov@users.noreply.github.com>
2022-10-04 14:27:33 +0300

    pirate parser

---
+++ fixer.pas
@@ -0,0 +1,10 @@
+﻿##
+  var KV := KV&<string, string>;
+  var replaces := Dict(
+    KV(#13#10'Tlgm: @it_boooks', ''),
+    KV('«', ''''), KV('»', ''''),
+    KV('…… ', '    '), KV('……', '    '),
+    KV('… ', '    '), KV('…', ''),
+    KV('. ', '.'), KV(' (', '('));
+  var fix: string -> string := s -> begin Result := s; foreach var kv in replaces do Result := Result.Replace(kv.Key, kv.Value); end;
+  WriteAllText('fixcode.py', fix(ReadAllText('fixcode.py', Encoding.GetEncoding(10007))), Encoding.UTF8);
